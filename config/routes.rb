2b8ef9f04fd4f0f3f7188059cc3fbe91fa09a462
Atticus Christensen <atticuschristensen@gmail.com>
2020-04-12 13:22:26 -0400

    made signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   get 'users/new'
   root 'fixed_pages#home'
+  
   match '/about',    to: 'fixed_pages#about',    via: 'get'
+  
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
