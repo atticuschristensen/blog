76a1b9d02e15759a103d82e6e7d58ab86a1917b1
Atticus Christensen <atticuschristensen@gmail.com>
2020-04-13 11:44:21 -0400

    removed buttons from navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       #sign_in(user)
       session[:user_id] = user.id
+      flash[:success]= "Signed in as " + user.username
       redirect_to root_url
-     else
-       redirect_to signup_url
+    else
+      flash[:danger] = "Login failed. Please check username and password are entered correct"
+       redirect_to signin_url
      end
      
      #sign_in(user)    
