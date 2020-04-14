f87decd2d12c35c74e95e87376e822ec26c1716d
Atticus Christensen <atticuschristensen@gmail.com>
2020-04-14 09:32:29 -0400

    new article is functional

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,19 @@
 class ArticlesController < ApplicationController
   def create
+    @article = current_user.articles.build(article_params)
+    @article.save
   end
   
   
  def new
    @article = Article.new
   end
-  
+
+
+ 
+ private
+ def article_params
+   params.require(:article).permit(:title, :body, :user_id)
+ end
+ 
 end
