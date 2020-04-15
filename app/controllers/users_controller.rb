74681264d6a89d2a6e2e137a6c5d502536617f15
Atticus Christensen <atticuschristensen@gmail.com>
2020-04-15 10:56:45 -0400

    vallidates password and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save 
-    session[:user_id]=@user.id
-    redirect_to root_url
+    if @user.save 
+      session[:user_id]=@user.id
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
   
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   before_save {username.downcase!}
   
   has_secure_password
-  validates :username, uniqueness:{ case_sensitive: false }
+  validates :username, presence: true, uniqueness:{ case_sensitive: false }
 
 
 end
